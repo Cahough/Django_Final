d5c9da9d5e6aa56d48138ee09a8e3f80cf768c79
Chough <carter.hough@hotmail.com>
2016-04-26 20:40:51 -0600

    Added urls file for do_action, and started on delete in do_action/views.py

--- do_action/views.py
+++ do_action/views.py
@@ -1,3 +1,21 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.http import HttpResponse, HttpResponseRedirect
+from list.models import my_contacts
 
-# Create your views here.
+def index(request):
+    pass
+
+def delete(request):
+    first_name = str(request.POST['first_name'])
+    last_name = str(request.POST['last_name'])
+    email = str(request.POST['email'])
+    phone = str(request.POST['phone'])
+
+    list = my_contacts.objects.filter(first__contains=first_name, last__contains=last_name, email__contains=email, phone__contains=phone).delete()
+    body = "<h4> Contact sucessfully deleted. </h4>"
+    return redirect('http://127.0.0.1:8000/')
+
+
+def udpate(request):
+    pass
+
